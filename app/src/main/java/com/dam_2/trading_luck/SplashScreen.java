a52e635f701df74555972044650e725a3bcd2703
Matri006 <119621038+Matri006@users.noreply.github.com>
2024-06-22 19:01:58 +0300

    Fourth

---
+++ app/src/main/java/com/dam_2/trading_luck/SplashScreen.java
@@ -0,0 +1,21 @@
+package com.dam_2.trading_luck;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
